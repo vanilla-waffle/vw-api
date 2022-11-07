d0ff29d9d97ea41f87d9e51d987a12ae0fe046dc
Arman Amirgaliyev <ar.amirgaliev.2015@gmail.com>
2022-11-07 16:46:35 +0600

    Merge branch 'dev' of C:\Users\arami\Desktop\Diploma Work\vw-api with conflicts.

---
+++ src/main/java/com/waffle/entity/Post.java
@@ -0,0 +1,13 @@
+package com.waffle.entity;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+//@Entity
+@Getter
+@Setter
+@Builder
+//@Table(name = "post")
+public class Post {
+}
---
+++ src/main/java/com/waffle/entity/Profile.java
@@ -0,0 +1,2 @@
+package com.waffle.entity;public class Profile {
+}
