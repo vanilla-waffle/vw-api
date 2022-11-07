8575642589f6d5ea1095886b9932f0f529bea89d
Arman Amirgaliyev <71120540+Armani020@users.noreply.github.com>
2022-11-07 18:20:03 +0600

    #1 Создание сущностей (#14)
    * Merge branch 'dev' of C:\Users\arami\Desktop\Diploma Work\vw-api with conflicts.
    
    * #1 Создание сущностей
    
    * #1 Исправил предупреждения checkstyle
    
    * #1 Переместил embedded сущность из Post в User
    
    * Update src/main/java/com/waffle/entity/Profile.java
    
    Co-authored-by: Zhomartkhan Talgatuly <71120576+khanZhomart@users.noreply.github.com>
    
    * Update src/main/java/com/waffle/entity/Profile.java
    
    Co-authored-by: Zhomartkhan Talgatuly <71120576+khanZhomart@users.noreply.github.com>
    
    Co-authored-by: Zhomartkhan Talgatuly <71120576+khanZhomart@users.noreply.github.com>

---
+++ src/main/java/com/waffle/entity/Profile.java
@@ -0,0 +1,15 @@
+package com.waffle.entity;
+
+import javax.persistence.Embeddable;
+
+/**
+ * Embedded Profile entity.
+ */
+@Embeddable
+public class Profile {
+
+    private String email;
+    private String password;
+    private String firstName;
+    private String lastName;
+}
---
+++ src/main/java/com/waffle/entity/User.java
@@ -0,0 +1,25 @@
+package com.waffle.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+/**
+ * User entity.
+ */
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Embedded
+    private Profile profile;
+}
