79a88557e93f816ef06aa0ae6f5976ff3da44e42
Arman Amirgaliyev <71120540+Armani020@users.noreply.github.com>
2022-11-07 18:17:07 +0600

    Update src/main/java/com/waffle/entity/Profile.java
    Co-authored-by: Zhomartkhan Talgatuly <71120576+khanZhomart@users.noreply.github.com>

--- src/main/java/com/waffle/entity/Profile.java
+++ src/main/java/com/waffle/entity/Profile.java
@@ -8,7 +8,6 @@ import javax.persistence.Embeddable;
 @Embeddable
 public class Profile {
 
-    private String login;
     private String email;
     private String password;
     private String firstName;
