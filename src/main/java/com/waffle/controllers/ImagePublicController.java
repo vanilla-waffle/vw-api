1261d7c1d3844b9f8ee6bc56dd80b76409239683
khanZhomart <jtalgatuly@yandex.kz>
2023-05-14 20:27:53 +0600

    #37 Возможность получения jpeg-изображений

--- src/main/java/com/waffle/controllers/ImagePublicController.java
+++ src/main/java/com/waffle/controllers/ImagePublicController.java
@@ -6,6 +6,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import static org.springframework.http.MediaType.IMAGE_JPEG_VALUE;
+import static org.springframework.http.MediaType.IMAGE_PNG_VALUE;
+
 @Api("public/media")
 @RequiredArgsConstructor
 public class ImagePublicController {
@@ -17,7 +20,10 @@ public class ImagePublicController {
      * @param uuid {@link String}
      * @return array of {@code byte}
      */
-    @GetMapping(value = "/{uuid}", produces = "image/png")
+    @GetMapping(
+            value = "/{uuid}",
+            produces = { IMAGE_PNG_VALUE, IMAGE_JPEG_VALUE }
+    )
     public byte[] download(@PathVariable final String uuid) {
         return imagePublicService.download(uuid);
     }
